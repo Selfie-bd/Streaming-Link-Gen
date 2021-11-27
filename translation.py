372ddb98f29255e460d185fc8901ef3e9132a0cc
Selfie SD <webtamil0@gmail.com>
2021-11-27 17:47:21 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,10 +1,32 @@
 class Translation(object):
-    START_TEXT = """Hey [{}](tg://user?id={}), I am Video Streaming Link Generator Bot,
+    START_TEXT = """Hey [{}](tg://user?id={}), 
 
-check /help to know more."""
+I am a Telegram File to Public\Stream link generator bot.
+
+I can generate Direct\Stream URL of any Telegram Files Or Videos for Downloading & Online Streaming like This - !!!
+
+☛ /help for More information
+
+NB:🔞 Don't forward Porn Files to me, You will Get Permanent B A N.
+
+Send me any file & see the magic!🤹."""
     DOWNLOAD_START = "📥Downloading📥"
     UPLOAD_START = "📤Uploading📤"
-    HELP_USER = """Hey [{}](tg://user?id={}), I am Video Streaming Link Generator Bot,
+    HELP_USER = """Hey [{}](tg://user?id={}),
+
+I think this Is Not That Though for U ...😅😅!
+
+⚡Sent Me Any Telegram File And I Will Provide You The The File Link...
+
+⚡This Link Can Be Used To Download Or Stream Files Through My Server
+
+⚡For Streaming Just Copy The Mono Link And Paste It In Your Video Player To Start Streaming
+
+⚡This bot shAres the Permanent Link to yOu.
+
+⚡Also I am Supported in Channels. Add me to Channel as Admin to Make Me Workable!
+
+For More Information: @groupdc,
 
 /jw - Stream any JWPlayer video .
 /brightcove - Stream any BrightCove video.
